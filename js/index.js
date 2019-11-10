7e1023ae76de1c2319ac97ed0148a733560f6adc
Mohamed Ahmed <mahmoud_mx14@yahoo.com>
2019-11-10 12:05:29 +0200

    mega menu

--- js/index.js
+++ js/index.js
@@ -2,74 +2,73 @@
 
 
 
-	$(function(){
+$(function() {
 
-	 //   	$('.nav_menu nav > ul > li > a').click(function(){
-	 //			$("html, body").animate({ scrollTop: $(this.hash).offset().top-100 }, 1000);
-	 //			return false;
-		//	});
-		
-			$('.top').click(function(){
-				$("html, body").animate({ scrollTop: 0 }, 1200);
-			});
+    //   	$('.nav_menu nav > ul > li > a').click(function(){
+    //			$("html, body").animate({ scrollTop: $(this.hash).offset().top-100 }, 1000);
+    //			return false;
+    //	});
+
+    $('.top').click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 1200);
+    });
 
 
 
-	    	$('.scroll-down').click(function(){
-	 			$("html, body").animate({ scrollTop: $('#about').offset().top+100 }, 1500);
-	 			return false;
-			});
+    $('.scroll-down').click(function() {
+        $("html, body").animate({ scrollTop: $('#about').offset().top + 100 }, 1500);
+        return false;
+    });
 
-			
 
-		});
-		
-		
-		
 
-            $(document).ready(function(){
+});
 
-                $(document).scroll(function() {
-                    var top = $(document).scrollTop();
-                    if (top > 300) {
-                        $(".top").addClass("top_show");
-                        $(".head-bar").addClass("head-sticky");
-                        $(".head-bar").addClass("anime");
-                      //  $(".header_top").addClass("paddin");
-                    }
-                    else if (top < 300) {
-                        $(".top").removeClass("top_show");
-                        $(".head-bar").removeClass("head-sticky");
-                        $(".head-bar").removeClass("anime");
-                       // $(".header_top").removeClass("paddin");
-                    } 
-                });
-            });
+
+
+
+$(document).ready(function() {
+
+    $(document).scroll(function() {
+        var top = $(document).scrollTop();
+        if (top > 300) {
+            $(".top").addClass("top_show");
+            $(".head-bar").addClass("head-sticky");
+            $(".head-bar").addClass("anime");
+            //  $(".header_top").addClass("paddin");
+        } else if (top < 300) {
+            $(".top").removeClass("top_show");
+            $(".head-bar").removeClass("head-sticky");
+            $(".head-bar").removeClass("anime");
+            // $(".header_top").removeClass("paddin");
+        }
+    });
+});
 
 
 var list = $("nav>ul li > a"); //Liste de tout les liens
 //Gestion du clique sur le boutton des trois bars afin d'afficher le menu dans les support avec un width <769
-$("nav > a").click(function(event){
+$("nav > a").click(function(event) {
     $("nav>ul").slideToggle();
-		return false;
+    return false;
 });
 
 //Gestion des cliques sur les liens avec élimination du comportement par défaut du a dans le cas où il existe un sous menu
-list.click(function (event) {
+list.click(function(event) {
     var submenu = this.parentNode.getElementsByTagName("ul").item(0);
     //S'il existe un sous menu sinon c'est un lien terminal
-    if(submenu!=null){
+    if (submenu != null) {
         event.preventDefault();
         $(submenu).slideToggle();
     }
 });
 //Gestion du resize de la fenetre pour eliminer le style ajouté par la méthode .slideToggle()
 
-$(window).resize(function () {
+$(window).resize(function() {
     if ($(window).width() > 767) {
         $("nav > ul, nav > ul  li  ul").removeAttr("style");
         $("nav").addClass("style0");
-		
+
     }
 });
 
@@ -77,19 +76,15 @@ $(window).resize(function () {
 
 $(window).load(function() {
 
-  $('.fancyTabs').each(function() {
-    var highestBox = 0;
-    $('.fancyTab a', this).each(function() {
+    $('.fancyTabs').each(function() {
+        var highestBox = 0;
+        $('.fancyTab a', this).each(function() {
 
-      if ($(this).height() > highestBox)
-        highestBox = $(this).height();
+            if ($(this).height() > highestBox)
+                highestBox = $(this).height();
+        });
+
+        $('.fancyTab a', this).height(highestBox);
+
     });
-
-    $('.fancyTab a', this).height(highestBox);
-
-  });
 });
-
-
-
-
